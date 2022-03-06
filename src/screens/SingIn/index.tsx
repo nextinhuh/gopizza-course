9f8b60778bf1d7b73a18ffa0f512bb3175a5ac00
Alvaro <alvaroneto.dev@gmail.com>
2022-03-06 15:19:31 -0300

    input component implemented

--- src/screens/SingIn/index.tsx
+++ src/screens/SingIn/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
+import { Input } from '@components/Input/indext';
+
 import { Container } from './styles';
 
 export function SingIn() {
     return (
         <Container>
+            <Input
+                placeholder='E-amil'
+                type='secondary'
+                autoCorrect={false}
+                autoCapitalize='none'
+            />
 
+            <Input
+                placeholder='Senha'
+                type='secondary'
+                secureTextEntry
+            />
         </Container>
     )
 }
